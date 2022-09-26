4ba05ba097a172e6cdc18f2ecdc9f8b76dbd34d5
ClearLove97 <114400838+ClearLove97@users.noreply.github.com>
2022-09-26 22:57:17 +0900

    Delete hello directory

--- hello/src/main/java/hello/core/member/MemberService.java
+++
@@ -1,7 +0,0 @@
-package hello.core.member;
-
-public interface MemberService {
-	void joun(Member member);
-	
-	Member findMember(Long MemberId);
-}
--- hello/src/main/java/hello/core/order/OrderService.java
+++
@@ -1,5 +0,0 @@
-package hello.core.order;
-
-public interface OrderService {
-	Order createOreder(Long memberId, String itemName , int itemPrice);
-}
